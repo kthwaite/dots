94cde04196001a5fd81d37fb8ffe18cf120d09ee
kthwaite <28732700+kthwaite@users.noreply.github.com>
2024-09-14 11:44:59 +0300

    feat(nvim): Add copilot/lua switch

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,14 @@
+local use_copilot_lua = os.getenv("K6E_COPILOT_LUA") == "1"
 return {
 	{
 		"github/copilot.vim",
 		event = "InsertEnter",
+		enabled = not use_copilot_lua,
+	},
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		enabled = use_copilot_lua,
 	},
 }
