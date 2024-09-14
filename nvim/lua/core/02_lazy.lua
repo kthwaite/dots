997ed5fa15d30ffeec7f7c1aefd850b71b381c0e
kthwaite <28732700+kthwaite@users.noreply.github.com>
2024-09-14 11:45:20 +0300

    feat,refactor(nvim): Add additional plugin sourcing

--- nvim/lua/core/02_lazy.lua
+++ nvim/lua/core/02_lazy.lua
@@ -1,66 +1,90 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 local uv = vim.uv or vim.loop
 if not uv.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "--single-branch",
-        "https://github.com/folke/lazy.nvim.git",
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--single-branch",
+		"https://github.com/folke/lazy.nvim.git",
+		lazypath,
+	})
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 local default_spec = {
-    { import = "plugins.core" },
-    { import = "plugins.extras" },
+	{ import = "plugins.core" },
+	{ import = "plugins.extras" },
 }
+
+---Get a list of plugins from a comma-separated list.
+---@param plugins string
+---@return string[]
+local function plugins_from_string(plugins)
+	local plugin_list = {}
+	for plugin in plugins:gmatch("[^,]+") do
+		plugin = string.gsub(plugin, "^%s*(.-)%s*$", "%1")
+		if plugin ~= "" then
+			table.insert(plugin_list, plugin)
+		end
+	end
+
+	return plugin_list
+end
+
+---Get a list of plugins from a comma-separated list in an environment variable.
+---@param env_var string
+---@return string[]
+local function plugins_from_env(env_var)
+	local plugins = os.getenv(env_var)
+	if plugins == nil then
+		return {}
+	end
+	return plugins_from_string(plugins)
+end
+
+---Get a list of extra plugins from a comma-separated list in K6E_NVIM_SESSION_PLUGINS.
+---@return string[]
+local function get_session_plugins()
+	return plugins_from_env("K6E_NVIM_SESSION_PLUGINS")
+end
 
 ---Get a list of extra plugins from a comma-separated list in K6E_NVIM_EXTRA_PLUGINS.
 ---@return string[]
 local function get_extra_plugins()
-    local extra_plugins = os.getenv("K6E_NVIM_EXTRA_PLUGINS")
-    if extra_plugins == nil then
-        return {}
-    end
-    local plugins = {}
-    for plugin in extra_plugins:gmatch("[^,]+") do
-        plugin = string.gsub(plugin, "^%s*(.-)%s*$", "%1")
-        if plugin ~= "" then
-            table.insert(plugins, plugin)
-        end
-    end
-    return plugins
+	return plugins_from_env("K6E_NVIM_EXTRA_PLUGINS")
 end
 
 ---Build a spec, interpolating extra plugins into the default spec.
----@param extra_plugins? string[]
+---@param ... string[]
 ---@return table
-local function build_spec(extra_plugins)
-    local spec = default_spec
-    if extra_plugins ~= nil then
-        for _, plugin in ipairs(extra_plugins) do
-            local resolved_name = "plugins." .. plugin
-            local ok, _ = pcall(require, resolved_name)
-            if not ok then
-                vim.notify("Failed to load plugin: " .. resolved_name, vim.log.levels.ERROR)
-            else
-                -- vim.notify("Loading extra plugin: " .. resolved_name, vim.log.levels.DEBUG)
-                table.insert(spec, { import = resolved_name })
-            end
-        end
-    end
-    return spec
+local function build_spec(...)
+	local spec = default_spec
+	for _, extra_plugins in ipairs({ ... }) do
+		if extra_plugins ~= nil then
+			for _, plugin in ipairs(extra_plugins) do
+				local resolved_name = "plugins." .. plugin
+				local ok, _ = pcall(require, resolved_name)
+				if not ok then
+					vim.notify("Failed to load plugin: " .. resolved_name, vim.log.levels.ERROR)
+				else
+					-- vim.notify("Loading extra plugin: " .. resolved_name, vim.log.levels.DEBUG)
+					table.insert(spec, { import = resolved_name })
+				end
+			end
+		end
+	end
+	return spec
 end
 
 local M = {}
 M.lazypath = lazypath
 M.extra_plugins = get_extra_plugins()
-M.spec = build_spec(M.extra_plugins)
+M.session_plugins = get_session_plugins()
+M.spec = build_spec(M.extra_plugins, M.session_plugins)
 
 require("lazy").setup({
-    spec = M.spec,
+	spec = M.spec,
 })
 
 return M
