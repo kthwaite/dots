7d630b17f4ed96853cc336ab5623f41ee305bc38
kthwaite <28732700+kthwaite@users.noreply.github.com>
2024-09-17 19:53:03 +0100

    feat(nvim): Some changes to allowed filetypes for treesitter and supermaven

--- nvim/lua/plugins/core/treesitter.lua
+++ nvim/lua/plugins/core/treesitter.lua
@@ -1,55 +1,56 @@
 local ensure_filetypes = {
-	"bash",
-	"javascript",
-	"lua",
-	"markdown",
-	"python",
-	"regex",
-	"tsx",
-	"typescript",
-	"vim",
-	"wgsl",
+    "bash",
+    "c",
+    "cpp",
+    "go",
+    "helm",
+    "javascript",
+    "json",
+    "just",
+    "kdl",
+    "lua",
+    "markdown",
+    "python",
+    "regex",
+    "rust",
+    "tsx",
+    "typescript",
+    "vim",
+    "wgsl",
+    "yaml",
+    "zig",
 }
 
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		lazy = true,
-		ft = ensure_filetypes,
-		cmd = { "TSInstall" },
-		config = function()
-			require("nvim-treesitter.configs").setup({
-				ensure_installed = ensure_filetypes,
-				highlight = {
-					enable = true,
-				},
-			})
-			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-			parser_config.just = {
-				install_info = {
-					url = "https://github.com/IndianBoy42/tree-sitter-just",
-					files = { "src/parser.c", "src/scanner.cc" },
-					branch = "main",
-					-- use_makefile = true, -- this may be necessary on MacOS (try if you see compiler errors)
-				},
-				maintainers = { "@IndianBoy42" },
-			}
-		end,
-	},
-	{
-		"danymat/neogen",
-		config = function()
-			require("neogen").setup({ snippet_engine = "luasnip" })
-		end,
-		keys = {
-			{ "<leader>gn", "<cmd>Neogen<cr>", desc = "Neogen" },
-		},
-	},
-	-- splitting/joining blocks of code using treesitter
-	--[[{
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        lazy = true,
+        ft = ensure_filetypes,
+        cmd = { "TSInstall" },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = ensure_filetypes,
+                highlight = {
+                    enable = true,
+                },
+            })
+            -- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        end,
+    },
+    {
+        "danymat/neogen",
+        config = function()
+            require("neogen").setup({ snippet_engine = "luasnip" })
+        end,
+        keys = {
+            { "<leader>gn", "<cmd>Neogen<cr>", desc = "Neogen" },
+        },
+    },
+    -- splitting/joining blocks of code using treesitter
+    --[[{
 		"Wansmer/treesj",
 		requires = { "nvim-treesitter/nvim-treesitter" },
 	},]]
-	--
+    --
 }
