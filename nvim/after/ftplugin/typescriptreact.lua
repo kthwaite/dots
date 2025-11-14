a7b359ca41549d253dd6c034e260189e7eec2182
Kit Thwaite <28732700+kthwaite@users.noreply.github.com>
2025-11-14 17:35:56 +0000

    feta: Add typescriptreact ftplugin

---
+++ nvim/after/ftplugin/typescriptreact.lua
@@ -0,0 +1,4 @@
+vim.opt.smarttab = true -- insert shiftwidth blanks
+vim.opt.shiftwidth = 2 -- width of shift, spaces
+vim.opt.softtabstop = 2 -- number of tab-spaces while editing
+vim.opt.tabstop = 2 -- tabs are worth 2 spaces
