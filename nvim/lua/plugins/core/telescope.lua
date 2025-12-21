17275ff9f7f5d1016650e58583d05940b0604412
Kit Thwaite <28732700+kthwaite@users.noreply.github.com>
2025-12-21 21:44:22 +0000

    refactor(nvim): Bump telescope version, add config file search

--- nvim/lua/plugins/core/telescope.lua
+++ nvim/lua/plugins/core/telescope.lua
@@ -1,10 +1,18 @@
+local n = require("core.utility").nnoremap
 return {
 	-- # telescope
 	{
 		"nvim-telescope/telescope.nvim",
 		lazy = true,
 		cmd = "Telescope",
-		branch = "0.1.x",
+		tag = "v0.2.0",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			n("<space>ec", function()
+				require("telescope.builtin").find_files({
+					cwd = vim.fn.stdpath("config"),
+				})
+			end, { desc = "Search over config files" })
+		end,
 	},
 }
